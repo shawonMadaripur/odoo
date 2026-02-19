59fe7c2d223b64ceaf2da5f8216d6fdb35fd21a9
shawon <saifulislamshawon1234@gmail.com>
2026-02-19 09:56:13 +0600

    set github identifier

---
+++ Github.py
@@ -0,0 +1,32 @@
+Create Personal Access Token
+
+1: Go to GitHub
+
+2: Click your profile → Settings
+
+3: Go to Developer settings
+
+4: Click Personal access tokens
+
+5: Click Tokens (classic)
+
+6: Click Generate new token (classic)
+
+7: Select:
+
+    ✅ repo (very important)
+
+8: Generate token
+
+9: Copy the token
+
+
+
+#######################################################################
+
+1 : Setup github identifier in terminal ?
+=> git config --global user.name "Saiful Islam Shawon"
+=> git config --global user.email "your_email@example.com"
+
+2 : Check git configuration ?
+=> git config --list
