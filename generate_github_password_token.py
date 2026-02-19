d1d2215c0f4cd79d9a640734544b14a75e290078
shawon <saifulislamshawon1234@gmail.com>
2026-02-19 09:45:54 +0600

    Generate github token

---
+++ generate_github_password_token.py
@@ -0,0 +1,21 @@
+Create Personal Access Token
+
+1: Go to GitHub
+
+2: Click your profile → Settings
+
+3: Go to Developer settings
+
+4: Click Personal access tokens
+
+5: Click Tokens (classic)
+
+6: Click Generate new token (classic)
+
+7: Select:
+
+    ✅ repo (very important)
+
+8: Generate token
+
+9: Copy the token
